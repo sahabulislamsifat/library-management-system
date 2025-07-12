c53508f66b63c88dc47d0f75351be9e5264d1b8f
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-07-13 03:04:50 +0600

    resolved some issues

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,6 +6,16 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:9000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
