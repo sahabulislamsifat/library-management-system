2824e1b33bbe72a7de7780105a66c9670aa7c7d6
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-07-13 02:50:28 +0600

    linked server api in baseURL

--- client/src/config/baseURL.ts
+++ client/src/config/baseURL.ts
@@ -1,2 +1,3 @@
 // export const BaseURL = "http://localhost:9000/api";
-export const BaseURL = "http://localhost:9000/api";
+export const BaseURL =
+  "https://library-management-api-with-mongoos.vercel.app/api";
